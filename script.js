f1c04ac660aa0f0a2277b13551ccb5d6e9ac8573
dhimanharsh <harshdhiman484@gmail.com>
2024-12-28 19:15:56 +0530

    add the validation script

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ let btn = document.querySelector(".check");
 let x = document.querySelector("#day");
 let y = document.querySelector("#month");
 let z = document.querySelector("#year");
+
 function ageCalculator() {
   let day = Number(x.value);
-  let month = Number(y.value)
+  let month = Number(y.value);
   let year = Number(z.value);
+
+  if (day > 31) {
+    window.alert("Day cannot be greater than 31.");
+    return;
+  }
+  if (month > 12) {
+    window.alert("Month cannot be greater than 12.");
+    return;
+  }
+  if (year > currentYear) {
+    window.alert("Year cannot be greater than the current year.");
+    return;
+  }
 
   let date = new Date().getDate();
   let mont = new Date().getMonth() + 1;
@@ -15,10 +29,8 @@ function ageCalculator() {
   let resultTwo = Math.abs(mont - month);
   let resultThree = Math.abs(currentYear - year);
 
-  let a =document.getElementsByTagName('h2')[0].innerText=resultThree
-  let b =document.getElementsByTagName('h2')[1].innerText=resultTwo
-  let c =document.getElementsByTagName('h2')[2].innerText=resultOne
+  let a = (document.getElementsByTagName("h2")[0].innerText = resultThree);
+  let b = (document.getElementsByTagName("h2")[1].innerText = resultTwo);
+  let c = (document.getElementsByTagName("h2")[2].innerText = resultOne);
 }
 btn.addEventListener("click", ageCalculator);
-
-
